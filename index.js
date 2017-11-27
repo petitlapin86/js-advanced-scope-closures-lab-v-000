9ab362c121488ce3083920c5c3ea584140cd7a3e
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-27 20:34:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,18 +10,29 @@ function produceTipCalculator(amount){
   }
 }
 
-function produceDrivingRange(blockRange){
-  return function(startingBlock, endingBlock){
+function produceDrivingRange(blockRange){ //function with one argument
+  return function(startingBlock, endingBlock){ // returns a new function 
 
-    let start = parseInt(startingBlock);
+    let start = parseInt(startingBlock); // calculates if something is within range
     let end = parseInt(endingBlock);
     let distanceToTravel = Math.abs(end - start);
     let difference = blockRange - distanceToTravel;
 
     if( difference > 0){
-      return `within range by ${difference}`
+      return `within range by ${difference}` // returns either within range 
     } else {
-      return `${Math.abs(difference)} blocks out of range`
+      return `${Math.abs(difference)} blocks out of range` // or out of range
+    }
+  }
+}
+
+
+function createDriver(){
+  let driverId = 0 // driver count starts at zero inside of scope
+  return class { //returns a new class within the function 
+    constructor(name){ //constructor with argument of name 
+      this.id = ++driverId //adding to counter 1 for each new driver
+      this.name = name 
     }
   }
 }
